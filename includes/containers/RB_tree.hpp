22fe191a7b1749858a9343c37aa307664751cd0f
Estela ESTELA <eestela@e3r7p3.clusters.42paris.fr>
2022-11-15 17:45:56 +0100

    re ecole encore

--- includes/containers/RB_tree.hpp
+++ includes/containers/RB_tree.hpp
@@ -17,7 +17,13 @@ struct RB_tree
 
 		node_type	*root;
 		ode_type	*leaf;
-		
+
+	
+
+	node_type	*rightest_node()	const	{	return (root->rightest_node());	}
+	node_type	*leftest_node()		const	{	return (root->leftest_node());	}
+
+
 
 
 };
---
+++ includes/containers/iterators/map_iterator.hpp
@@ -0,0 +1,69 @@
+#ifndef MAP_ITERATOR_HPP
+# define MAP_ITERATOR_HPP
+
+
+#include "iterator_trait.hpp"
+#include "../RB_node.hpp"
+# include "../../utlis.hpp"
+
+namespace ft
+{
+
+template <bool isConst, class T>
+class map_iterator
+{
+public :
+	typedef	std::ptrdiff_t										difference_type;
+	typedef typename choose<isConst, const T, T>::type			value_type;
+	typedef	RB_node<value_type>									node_type;
+	typedef value_type*											pointer;
+	typedef value_type&											reference;
+	typedef std::bidirectional_iterator_tag						iterator_category;
+
+
+private:
+	node_type	*_node;
+
+public:
+	map_iterator() : _node(NULL)	{};
+	map_iterator(void *node) : _node(staticast<node_type*>(node))	{};
+	map_iterator(const map_iterator &rhs) : _node(rhs._node)	{};
+	~map_iterator();
+
+map_iterator &operator=(const map_iterator *rhs)
+{
+	this->_node = rhs._node;
+	return (*this);
+}
+
+//operators ++ and --
+
+reference	operator*() const
+{	return *this->_node->value;	}
+
+pointer		operator->() const
+{	return this->_node->value;	}
+
+
+friend
+bool	operator==(const map_iterator &lhs, const map_iterator &rhs)
+{	return (lhs._node == rhs._node);	}
+
+friend
+bool	operator!=(const map_iterator &lhs, const map_iterator &rhs)
+{	return (lhs._node != rhs._node);	}
+
+node_type	*node()	const	{
+	return this->_node;
+}
+
+};
+
+
+
+
+}
+
+
+
+#endif
